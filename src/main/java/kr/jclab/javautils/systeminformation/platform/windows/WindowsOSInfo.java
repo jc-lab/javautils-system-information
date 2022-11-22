58d32cb9a970b2bcf5dfd184fcb9c474ed85f70b
hyun-duk choi <hyunduk.choi@gmail.com>
2022-11-22 14:51:17 +0900

    Check Windows 11 by Build Number

--- src/main/java/kr/jclab/javautils/systeminformation/platform/windows/WindowsOSInfo.java
+++ src/main/java/kr/jclab/javautils/systeminformation/platform/windows/WindowsOSInfo.java
@@ -6,27 +6,49 @@ import kr.jclab.javautils.systeminformation.model.OSInformation;
 import kr.jclab.javautils.systeminformation.osinfo.OSInfoBase;
 
 import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WindowsOSInfo implements OSInfoBase {
+    private final Pattern WINDOWS_10_PATTERN = Pattern.compile("Windows 10", Pattern.CASE_INSENSITIVE);
+
     @Override
     public OSInformation read() {
         TreeMap<String, Object> objects =  Advapi32Util.registryGetValues(WinReg.HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion");
         Object installDateObj = objects.get("InstallDate");
         Object productNameObj = objects.get("ProductName");
         Object releaseIdObj = objects.get("ReleaseId");
+        Object currentBuildNumberObj = objects.get("CurrentBuildNumber");
 
         long installDate = 0;
         if (installDateObj instanceof Long) {
             installDate = (Long) installDateObj;
-        } else if (installDateObj instanceof Integer) {
-            installDate = ((Integer) installDateObj).longValue();
+        } else if (installDateObj instanceof Number) {
+            installDate = ((Number) installDateObj).longValue();
+        }
+
+        int currentBuildNumber = 0;
+        if (currentBuildNumberObj instanceof String) {
+            currentBuildNumber = Integer.parseInt((String) currentBuildNumberObj);
+        } else if (currentBuildNumberObj instanceof Number) {
+            currentBuildNumber = ((Number) currentBuildNumberObj).intValue();
+        }
+
+        String productName = (String) productNameObj;
+
+        if (currentBuildNumber >= 22000) {
+            Matcher matcher = WINDOWS_10_PATTERN.matcher(productName);
+            if (matcher.find()) {
+                productName = matcher.replaceAll("Windows 11");
+            }
         }
 
         return OSInformation.builder()
                 .identity("windows")
                 .installedAt(installDate)
-                .productName((String)productNameObj)
+                .productName(productName)
                 .releaseId((String)releaseIdObj)
+                .currentBuildNumber(currentBuildNumber)
                 .build();
     }
 }
