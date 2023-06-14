0ca0f0e03e30cba1fcb76a8d6d34e5655397791d
Joseph Lee <joseph@jc-lab.net>
2023-06-14 17:40:37 +0900

    feat: ci deploy (#7)
    * feat(ci): auto deploy
    * use setup-java

---
+++ build.gradle.kts
@@ -0,0 +1,95 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    `signing`
+}
+
+group = "kr.jclab.javautils"
+version = Version.PROJECT
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+    withJavadocJar()
+    withSourcesJar()
+}
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
+
+dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.20")
+    annotationProcessor("org.projectlombok:lombok:1.18.20")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.20")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.20")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+
+    implementation("org.apache.commons:commons-lang3:3.11")
+    implementation("commons-io:commons-io:2.8.0")
+    implementation("net.java.dev.jna:jna:5.6.0")
+    implementation("net.java.dev.jna:jna-platform:5.6.0")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                name.set(project.name)
+                description.set("cross-platform system information")
+                url.set("https://github.com/jc-lab/javautils-system-information")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("jclab")
+                        name.set("Joseph Lee")
+                        email.set("joseph@jc-lab.net")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/jc-lab/javautils-system-information.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/jc-lab/javautils-system-information.git")
+                    url.set("https://github.com/jc-lab/javautils-system-information")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            url = uri(if ("$version".endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+            credentials {
+                username = findProperty("ossrhUsername") as String?
+                password = findProperty("ossrhPassword") as String?
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
+}
+
+tasks.withType<Sign>().configureEach {
+    onlyIf { project.hasProperty("signing.gnupg.keyName") || project.hasProperty("signing.keyId") }
+}
